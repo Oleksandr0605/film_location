68b113f77ef5f5b6763dbf5d90f99257418f5b4f
Oleksandr Ivaniuk <ivaniuk.pn@ucu.edu.ua>
2023-02-18 10:41:28 +0200

    get location first done

--- map.py
+++ map.py
@@ -2,20 +2,24 @@ import argparse
 import folium
 from geopy.geocoders import Nominatim
 from haversine import haversine, Unit
-from geopy import GeocodeUnavailable
 
 
 def read_data(path: str) -> list:
     """
+    reads data from file about films and return list of tuples with year and the place where it was filmed
+
+    >>> print(read_data("locations.list")[1])
+    ('2015', 'Coventry, West Midlands, England, UK')
     """
     with open(path, "r") as fff:
         file = fff.readlines()
     file = file[15:]
     year = ""
     year_location = []
-    for jnd, film in enumerate(file):
+    for jnd, film in enumerate(file[:len(file)//1000]):
         for ind, symb in enumerate(film):
-            if symb == "(" and 49 <= ord(film[ind + 1]) <= 57:
+            if symb == "(" and 49 <= ord(film[ind + 1]) <= 57 and 48 <= ord(film[ind + 3]) <= 57 and\
+                (film[ind + 5] == ")" or film[ind + 5] == "/"):
                 year = film[ind + 1:ind + 5]
                 break
         file[jnd] = file[jnd].replace("\t", "|")
@@ -30,10 +34,28 @@ def read_data(path: str) -> list:
 
 def get_locations(year_location: list) -> list:
     """
+    gets locations of all films
+
+    >>> get_locations([('2000', 'Montenegro')])
+    [(42.9868853, 19.5180992)]
     """
-    for tpl in year_location:
-        locations.append(())
     locations = []
+    for tpl in year_location:
+        geolocator = Nominatim(user_agent="get_location")
+        tpl_split = tpl[1].split(",")
+        flag = True
+        for elm in tpl_split:
+            try:
+                location = geolocator.geocode(elm.strip())
+                flag = False
+                break
+            except:
+                continue
+        if not flag:
+            locations.append((location.latitude, location.longitude))
+        else:
+            continue
+    return locations
 
 
 def main():
@@ -46,10 +68,12 @@ def main():
     parser.add_argument("longitude", type=str)
     parser.add_argument("path_dataset", type=str)
     args = parser.parse_args()
-    
+
     year_location = read_data(args.path_dataset)
+
     year_location = [elm for elm in year_location if int(elm[0]) == args.year]
 
+    locations = get_locations(year_location)
 
     map = folium.Map(tiles="Stamen Terrain",
                 location=[args.latitude, args.longitude],
@@ -59,3 +83,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # import doctest
+    # print(doctest.testmod())
