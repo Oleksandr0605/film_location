ee752a980c7a95311d1334751a3e8b254a95ce2f
Oleksandr Ivaniuk <ivaniuk.pn@ucu.edu.ua>
2023-02-21 22:58:28 +0200

    end

--- map.py
+++ map.py
@@ -1,16 +1,18 @@
+"""
+Search nearest locations of films
+"""
 import argparse
 import folium
 from geopy.geocoders import Nominatim
-from haversine import haversine, Unit
-from math import fabs
+from haversine import haversine
 
 
-def read_data(path: str) -> list:
+def read_data(path: str, year_film: int) -> list:
     """
     reads data from file about films and return list of tuples with year and the place where it was filmed
 
-    >>> print(read_data("locations.list")[1])
-    ('2015', 'Coventry, West Midlands, England, UK')
+    >>> print(read_data("locations.list", 2015)[1])
+    ('2015', 'West Hills, California, USA')
     """
     with open(path, "r") as fff:
         file = fff.readlines()
@@ -19,17 +21,18 @@ def read_data(path: str) -> list:
     year_location = []
     for jnd, film in enumerate(file):
         for ind, symb in enumerate(film):
-            if symb == "(" and 49 <= ord(film[ind + 1]) <= 57:
+            if symb == "(" and 48 <= ord(film[ind + 1]) <= 57 and 48 <= ord(film[ind + 2]) <= 57 and\
+                48 <= ord(film[ind + 3]) <= 57 and 48 <= ord(film[ind + 4]) <= 57:
                 year = film[ind + 1:ind + 5]
                 break
         file[jnd] = file[jnd].replace("\t", "|")
         file[jnd] = file[jnd].replace("\n", "|")
         file[jnd] = file[jnd].split("|")
-        if file[jnd][-2][-1] != ")":
+        if file[jnd][-2][-1] != ")" and year_film == int(year):
             year_location.append((year, file[jnd][-2]))
-        else:
+        elif year_film == int(year):
             year_location.append((year, file[jnd][-3]))
-    return year_location
+    return year_location[:80]
 
 
 def get_locations(year_location: list) -> list:
@@ -51,7 +54,7 @@ def get_locations(year_location: list) -> list:
                 break
             except:
                 continue
-        if not flag and location != None:
+        if not flag and location is not None:
             locations.append((location.latitude, location.longitude))
         else:
             continue
@@ -62,16 +65,14 @@ def distance(location: tuple, locations: list) -> list:
     """
     get distances from your location to every location of films
 
-    >>> distance((53.0162014, -2.1812607), [(42.9868853, 19.5180992), (41.0671774, 29.0432713)])
+    >>> list(distance((53.0162014, -2.1812607), [(42.9868853, 19.5180992), (41.0671774, 29.0432713)]))
     [(1949.7846486566084, (42.9868853, 19.5180992)), (2682.981082694581, (41.0671774, 29.0432713))]
     """
-    distances = []
     for loc in locations:
         try:
-            distances.append((haversine(location, loc), loc))
+            yield (haversine(location, loc), loc)
         except:
             continue
-    return distances
 
 
 def main():
@@ -87,24 +88,29 @@ def main():
 
     location = (args.latitude, args.longitude)
 
-    year_location = read_data(args.path_dataset)
-
-    year_location = [elm for elm in year_location if int(elm[0]) == args.year]
+    year_location = read_data(args.path_dataset, args.year)
 
     locations = get_locations(year_location)
 
-    distances = distance(location, locations)
+    distances = list(distance(location, locations))
 
     distances = sorted(distances)
 
-    map = folium.Map(tiles="Stamen Terrain",
+    map = folium.Map(tiles="Stamen Toner",
                 location=list(location),
                 zoom_start=3)
+
     for ind in range(10):
         map.add_child(folium.Marker(distances[ind][1],
                                     icon=folium.Icon()))
+
+    map.add_child(folium.Marker(location,
+                                icon=folium.Icon(color='red')))
+
+    map.add_child(folium.LayerControl())
+
     map.save("map1.html")
-    
+
 
 if __name__ == "__main__":
     main()
